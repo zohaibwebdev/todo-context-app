8c0f996c9a291dea1d0a0810f05b48e94fb58efa
Muhammad Zohaib Ul Hassan <zohaibfas@gmail.com>
2023-03-09 11:32:32 +0500

    first commit

---
+++ src/components/AppContainer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Header";
+
+const AppContainer = () => {
+  return (
+    <div className="app-container">
+      <Header />
+    </div>
+  );
+};
+
+export default AppContainer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1 className="heading">Todo App</h1>
+      <h3 className="name">Adviced by Dr. Awais Shahid</h3>
+    </div>
+  );
+};
+
+export default Header;
